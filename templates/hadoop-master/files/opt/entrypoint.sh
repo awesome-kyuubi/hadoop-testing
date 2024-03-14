201bee79e3932123f95a3d70881e87e5d6a78bd9
zhouyifan279 <88070094+zhouyifan279@users.noreply.github.com>
2024-03-14 13:12:40 +0800

    Divide node files into groups (#73)
    * Divide node files into groups
    
    * Divide node files into groups
    
    * Divide node files into groups

---
+++ templates/hadoop-master/files/opt/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+"$@" &
+
+if [[ -v POST_BOOTSTRAP_COMMAND ]]; then
+    $POST_BOOTSTRAP_COMMAND
+fi
+
+if [[ -d /opt/service-ready.d ]]; then
+    for init_script in /opt/service-ready.d/*; do
+        "${init_script}"
+    done
+fi
+
+wait
