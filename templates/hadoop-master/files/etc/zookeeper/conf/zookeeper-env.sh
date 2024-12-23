cefc371428fdb8dc27577da7279d2a523e1b58a1
Cheng Pan <chengpan@apache.org>
2024-12-23 16:20:58 +0800

    ZOOKEEPER-1177

--- templates/hadoop-master/files/etc/zookeeper/conf/zookeeper-env.sh
+++ templates/hadoop-master/files/etc/zookeeper/conf/zookeeper-env.sh
@@ -1,1 +1,5 @@
 export ZK_SERVER_HEAP=384
+
+# ZOOKEEPER-1177 (3.6.0)
+# https://xie.infoq.cn/article/f346a8284f59e16bb7f89188e
+export SERVER_JVMFLAGS="-Dzookeeper.watchManagerName=org.apache.zookeeper.server.watch.WatchManagerOptimized"
